a84c2b42fa38c9712e8c1ab7bc46e36dca587cec
Andy Ruiz <46635513+ndrz069@users.noreply.github.com>
2022-01-10 14:56:35 +0100

    Initial

---
+++ mac_new_installation.sh
@@ -0,0 +1,19 @@
+brew cask install iterm2
+brew cask install lastpass
+brew cask install brave-browser
+brew cask install firefox
+brew cask install alfred
+brew cask install jdownloader
+brew cask install kindle
+brew cask install parallels
+brew cask install sublime-text
+brew cask install the-unarchiver
+brew cask install vlc
+brew cask install visual-studio-code
+brew cask install adobe-creative-cloud
+ 
+brew cask install font-source-code-pro
+ 
+brew cask install google-drive
+brew cask install google-drive-file-stream
+brew cask install carbon-copy-cloner
